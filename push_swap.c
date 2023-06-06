a7e3d07b2ff23b60c1bc67a48f789d4e71c17b00
William Ferreira Rocha <wiferrei@c1r2s1.42lisboa.com>
2023-06-06 19:58:13 +0100

    some changes

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,18 @@
 /*   By: wiferrei <wiferrei@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 19:06:43 by wiferrei          #+#    #+#             */
-/*   Updated: 2023/06/06 19:07:23 by wiferrei         ###   ########.fr       */
+/*   Updated: 2023/06/06 19:56:04 by wiferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
+
+int	main(int ac, char **av)
+{
+	t_stack_node	*a;
+	t_stack_node	*b;
+
+	a = NULL;
+	b = NULL;
+	return (0);
+}
--- push_swap.h
+++ push_swap.h
@@ -5,8 +5,24 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: wiferrei <wiferrei@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/06 19:07:18 by wiferrei          #+#    #+#             */
-/*   Updated: 2023/06/06 19:07:21 by wiferrei         ###   ########.fr       */
+/*   Created: 2023/06/06 19:43:49 by wiferrei          #+#    #+#             */
+/*   Updated: 2023/06/06 19:57:05 by wiferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <unistd.h>
+# include <stdarg.h>
+# include <stdlib.h>
+# include <limits.h>
+
+typedef struct s_stack_node
+{
+	int					nbr;
+	struct s_stack_node	*prev;
+	struct s_stack_node	*next;
+}					t_stack_node;
+
+#endif
