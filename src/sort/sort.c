a15d8d9f024a666648384e6bf755da0864c09dc8
William Ferreira Rocha <wiferrei@c2r10s2.42lisboa.com>
2023-06-27 08:54:30 +0100

    test

---
+++ src/sort/sort.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wiferrei <wiferrei@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/20 11:00:14 by wiferrei          #+#    #+#             */
+/*   Updated: 2023/06/27 08:52:35 by wiferrei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/push_swap.h"
+
+void	sort(t_stack_node **stack_a, t_stack_node **stack_b)
+{
+	int	mean;
+
+	while (stack_size(*stack_a) > 5)
+	{
+		mean = calculate_mean(*stack_a);
+		if ((*stack_a)->nbr < mean)
+			pb(stack_a, stack_b);
+		else if ((*stack_a)->nbr > mean)
+			ra(stack_a);
+	}
+
+	
+}
