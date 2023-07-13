c1afeb3d70cb31f1e969733a2a0a3d4f3ac4d991
William <william@pop-os.localdomain>
2023-07-13 19:01:55 +0100

    test phase

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,13 +6,11 @@
 /*   By: wiferrei <wiferrei@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 19:06:43 by wiferrei          #+#    #+#             */
-/*   Updated: 2023/07/12 20:40:52 by wiferrei         ###   ########.fr       */
+/*   Updated: 2023/07/13 18:57:59 by wiferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
-
-
 
 int	main(int ac, char **av)
 {
@@ -24,9 +22,12 @@ int	main(int ac, char **av)
 	if (ac < 2)
 		return (0);
 	ft_stack_init(av, &stack_a);
+	if (stack_a == NULL)
+		ft_putstr_fd("Error\n", 1);
 	if (a_is_sorted(stack_a))
 	{
 		ft_putstr_fd("List is already sorted\n", 1);
+		clear_stack(&stack_a);
 		return (0);
 	}
 	if (stack_size(stack_a) < 10)
@@ -35,8 +36,6 @@ int	main(int ac, char **av)
 	{
 		sort_medium_stack(&stack_a, &stack_b);
 	}
-	if (a_is_sorted(stack_a))
-		printf("Essa merda ta sorteada\n");
 	clear_stack(&stack_a);
 	clear_stack(&stack_b);
 	return (0);
